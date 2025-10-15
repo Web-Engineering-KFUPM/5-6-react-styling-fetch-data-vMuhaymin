31ce6b2813933f64cb5ca1ea768f4171e0fbb712
Abdulmuhaymin althabit <s202275600@kfupm.edu.sa>
2025-10-15 09:31:19 +0300

    Task 1 and 2

--- 5-6-react-styling-fetch-data/src/components/UserCard.jsx
+++ 5-6-react-styling-fetch-data/src/components/UserCard.jsx
@@ -5,20 +5,26 @@ function UserCard({ user, onUserClick }) {
   return (
     <Card className="user-card">
       <Card.Body>
-        <div>
-          <div className="user-avatar">
+
+        <div className="d-flex align-items-center mb-3">
+          <div className="user-avatar me-3">
             {user.name.charAt(0)}
           </div>
         </div>
 
-        <Card.Title>{user.name}</Card.Title>
-        <Card.Text>
+        <Card.Title className="mb-0"  >{user.name}</Card.Title>
+        <Card.Text className="mb-3">
           <strong>Username:</strong> {user.username}<br />
           <strong>Email:</strong> {user.email}<br />
           <strong>Phone:</strong> {user.phone}
         </Card.Text>
       {/* Buttotn*/}
-      
+              
+      <Button onClick={() => onUserClick(user)}>
+        View Details
+        </Button>
+
+
       </Card.Body>
     </Card>
   )
